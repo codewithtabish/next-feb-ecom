42983fb6092da4cf413eceaafdecdf4f0359570b
codewithtabish <codewithtabish16@gmail.com>
2025-02-06 01:10:38 +1100

    added extra content

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,6 +6,7 @@ const BlogPage = () => {
          
 
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos nulla, laboriosam, eaque et esse ipsa ratione dolore, expedita commodi facere magni! Eligendi quis debitis quae amet, fugiat dignissimos. Eum, dolor!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium odio blanditiis iste tempore vitae libero voluptatibus eius? Distinctio modi, quisquam doloremque assumenda ullam quidem ad, temporibus, asperiores reprehenderit ut libero.
         </div>
     )
 }
