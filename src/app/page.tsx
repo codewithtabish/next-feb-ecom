4ae57e99005101aa1dadf18ac02674dd88affce6
codewithtabish <codewithtabish16@gmail.com>
2025-02-06 22:27:49 +1100

    added more content in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ const HomePage = () => {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque labore unde voluptates aspernatur expedita doloremque! Necessitatibus possimus ex aut rem sunt quaerat voluptas, eligendi, natus, voluptates dicta quam quasi placeat.
 
      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi non possimus totam unde temporibus ipsa doloribus, quas illum molestiae quo corrupti est facilis, laudantium perferendis at! In doloremque placeat animi!
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum et minus iusto accusamus consequatur veritatis dolore aliquam omnis, rerum incidunt veniam suscipit voluptates expedita corporis cum itaque magnam eius. Dolorum!
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum et minus iusto accusamus consequatur veritatis dolore aliquam omnis, rerum incidunt veniam suscipit voluptates expedita corporis cum itaque magnam eius. Dolorum!
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum et minus iusto accusamus consequatur veritatis dolore aliquam omnis, rerum incidunt veniam suscipit voluptates expedita corporis cum itaque magnam eius. Dolorum!
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum et minus iusto accusamus consequatur veritatis dolore aliquam omnis, rerum incidunt veniam suscipit voluptates expedita corporis cum itaque magnam eius. Dolorum!
       </div>
 
     </>
