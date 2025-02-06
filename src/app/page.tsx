647caa368bb2d5e03c3131854fbeb3af22cd6159
codewithtabish <codewithtabish16@gmail.com>
2025-02-06 16:25:14 +1100

    added images in public dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ const HomePage = () => {
   return (
     <>
       <div className="grid-background"></div>
+      <div className='mt-5'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque labore unde voluptates aspernatur expedita doloremque! Necessitatibus possimus ex aut rem sunt quaerat voluptas, eligendi, natus, voluptates dicta quam quasi placeat.
 
      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi non possimus totam unde temporibus ipsa doloribus, quas illum molestiae quo corrupti est facilis, laudantium perferendis at! In doloremque placeat animi!
+      </div>
+
     </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,7 +21,7 @@ export default {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#305cde',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
