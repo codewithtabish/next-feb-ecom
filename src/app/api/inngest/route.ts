2581260dd9646287f5935800fc7fc75056963882
codewithtabish <codewithtabish16@gmail.com>
2025-02-06 23:06:47 +1100

    updated route file

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,9 +2,12 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { checkSaveAndSendUpdateEmailToUser } from "@/inngest/functions";
 
+export const runtime='edge'
+
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  streaming:'allow',
   functions: [
     checkSaveAndSendUpdateEmailToUser
     /* your functions will be passed here later! */
