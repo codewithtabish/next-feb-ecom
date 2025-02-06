a87d9ecb5cdbd38e4efe047f3e636a65dc2b0fe2
codewithtabish <codewithtabish16@gmail.com>
2025-02-06 23:10:41 +1100

    updated route file again

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,7 +2,7 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { checkSaveAndSendUpdateEmailToUser } from "@/inngest/functions";
 
-export const runtime='edge'
+// export const runtime='edge'
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
