ca75a5ec5722887876aeda842fe1f62df084e50d
codewithtabish <codewithtabish16@gmail.com>
2025-02-08 15:21:57 +1100

    updated the eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,11 @@ const eslintConfig = [
     ...compat.extends("next/core-web-vitals", "next/typescript"),
     {
         rules: {
-            "react/no-unescaped-entities": "off", // Disable the rule
+            "react/no-unescaped-entities": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "import/no-anonymous-default-export": "off",
+            "react-hooks/exhaustive-deps": "off"
         },
     },
 ];
